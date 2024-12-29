59943b5b7f8ff3fdca03793b03c2eb80836a46d7
marisamak <marinasamakova1507@gmail.com>
2024-12-29 12:25:17 +0300

    add

---
+++ 3_3/main.py
@@ -0,0 +1,75 @@
+from tank import Tank
+from tkinter import*
+import world
+import tanks_collection
+import texture
+
+def key_press(event):
+    print(f'ажата клавиша {event.keysym}, код {event.keycode}')
+
+KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN = 37,39,38,40
+
+KEY_W=87
+KEY_S=83
+KEY_A=65
+KEY_D=68
+FPS=60
+
+def update():
+    tanks_collection.update()#####
+    player=tanks_collection.get_player()
+    world.set_camera_xy(player.get_x()-world.SCREEN_WIDTH//2+player.get_size()//2,
+                        player.get_y()-world.SCREEN_HEIGHT//2+player.get_size()//2)
+    world.update_map()
+    w.after(1000//FPS,update)
+
+
+
+
+def key_press(event):
+    player=tanks_collection.get_player()
+    if event.keycode == KEY_W:
+        player.forvard()
+    if event.keycode == KEY_S:
+        player.backward()
+    if event.keycode == KEY_A:
+        player.left()
+    if event.keycode == KEY_D:
+        player.right()
+
+    #if event.keycode == KEY_UP:
+        #world.move_camera(0,-5)
+    #if event.keycode == KEY_DOWN:
+        #world.move_camera(0,5)
+    #if event.keycode == KEY_LEFT:
+        #world.move_camera(-5,0)
+    #if event.keycode == KEY_RIGHT:
+        #world.move_camera(5,0)
+    if event.keycode == 32:
+        tanks_collection.spawn()
+
+def load_textures():
+    texture.load('tank_up',
+                 '../img/tankT34_up.png')
+    texture.load("tank_down",
+                 '../img/tankT34_down.png')
+    texture.load("tank_left",
+                 '../img/tankT34_left.png')
+    texture.load("tank_right",
+                 '../img/tankT34_right.png')
+
+    texture.load(world.BRICK, "../img/brick.png")
+    texture.load(world.WATER, "../img/water.png")
+    texture.load(world.CONCRETE, "../img/wall.png")
+    texture.load(world.MISSLE, "../img/bonus.png")
+
+w=Tk()
+load_textures()
+w.title('Танки на минималках 2.0')
+canv=Canvas(w, width=world.SCREEN_WIDTH, height=world.SCREEN_HEIGHT, bg='#8ccb5e')
+canv.pack()
+world.initialaze(canv)
+tanks_collection.initialize(canv)
+w.bind('<KeyPress>', key_press)
+update()
+w.mainloop()
---
+++ 3_3/tanks_collection.py
@@ -0,0 +1,48 @@
+from random import randint
+from tank import Tank
+import world
+
+_tanks = []
+_canvas = None
+
+def initialize(canv):
+    global _canvas
+    _canvas = canv
+
+    spawn(False)
+    for i in range(2):
+        spawn(True).set_target(get_player())
+
+    print(_tanks)
+
+def get_player():
+    return _tanks[0]
+
+def update():
+    for tank in _tanks:
+        tank.update()
+        check_collision(tank)
+
+def check_collision(tank):
+    for other_tank in _tanks:
+        if tank == other_tank:
+            continue
+        if tank.inersects(other_tank):
+            return True
+    return False
+
+def spawn(is_bot=True):
+    cols = world.get_cols()
+    rows = world.get_rows()
+
+    while True:
+        col = randint(1, cols-1)
+        row = randint(1, rows-1)
+
+        if world.get_block(row, col) != world.GROUND:
+            continue
+
+        t = Tank(_canvas, x = col*world.BLOCK_SIZE, y = row*world.BLOCK_SIZE, speed=2, bot=is_bot)
+        if not check_collision(t):
+            _tanks.append(t)
+            return t
